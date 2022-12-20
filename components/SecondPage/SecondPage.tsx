a1e7bd11e8d733d0cd8c62f52fc123f30e5671ae
carolinavaleriano <102779256+carolinavaleriano@users.noreply.github.com>
2022-12-20 16:59:36 -0300

    second page and fixing bugs

--- components/SecondPage/SecondPage.tsx
+++ components/SecondPage/SecondPage.tsx
@@ -10,25 +10,31 @@ function SecondPage() {
       </div>
       <div className={styles.card_container}>
         <div className={styles.card}>
+          <h1>1</h1>
           <h2>Cultura Maker</h2>
           <h3>
             Além de aprender em equipe, os estagiários também atuam na criação
             de um produto digital.
           </h3>
+          <button>Saiba mais</button>
         </div>
         <div className={styles.card}>
+          <h1>2</h1>
           <h2>Trilhas de Conhecimento</h2>
           <h3>
             Os conhecimentos técnicos são compartilhados por meio de trilhas,
             conectadas com nossos pilates de engenharia, design e produtos.
           </h3>
+          <button>Saiba mais</button>
         </div>
         <div className={styles.card}>
+          <h1>3</h1>
           <h2>Soft Skills</h2>
           <h3>
             São realizador workshops para os estagiários aprenderem também sobre
             habilidades comportamentais como liderança, empatia e inovação.
           </h3>
+          <button>Saiba mais</button>
         </div>
       </div>
     </div>
